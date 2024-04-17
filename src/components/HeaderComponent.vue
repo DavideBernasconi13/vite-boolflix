84c244e26babd6be590dd63540da7ddbb0d71485
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-17 15:26:03 +0200

    delete button element commentato

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,7 +3,6 @@
         <a class="navbar-brand">Boolflix</a>
         <form class="form-inline d-flex">
             <input class="form-control mr-sm-2" type="search" placeholder="Inserisci una parola" aria-label="Search">
-            <!-- <button class="btn btn-outline-success my-2 my-sm-0" >Cerca</button> -->
             <button class="btn btn-outline-light mx-2" type="submit">Cerca</button>
         </form>
     </nav>
