9f3b120284330bd2af0f4654b26e2d4c8e3b5f58
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-17 15:34:38 +0200

    create MainComponent and import in App.vue

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <main class="bg-secondary">
+        Main component
+    </main>
+
+</template>
+
+<script>
+export default {
+    name: 'MainComponent'
+
+}
+</script>
+
+<style lang="scss" scoped>
+main {
+    height: calc(100vh - 60px);
+}
+</style>
