9b658814aee52e230f92e0fb7709b64d32af5785
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-04-18 09:42:42 +0200

    add keyup.enter su input per chiamare l'evento formSearch

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -3,7 +3,7 @@
         <a class="navbar-brand">Boolflix</a>
         <div class="form-inline d-flex">
             <input v-model="store.query" class="form-control mr-sm-2" type="search" placeholder="Inserisci una parola"
-                aria-label="Search">
+                aria-label="Search" @keyup.enter="$emit('formSearch')">
             <button class="btn btn-outline-light mx-2" @click="$emit('formSearch')">Cerca</button>
         </div>
     </nav>
